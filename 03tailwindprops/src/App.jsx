498f3e5af0b3ff59a409911849b33ee04ba0d8fa
sumit kumar <sumit1523@gmail.com>
2024-02-27 20:45:37 +0530

    tailwindand props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  // console.log(props, "props-App");
+  let obj = {
+    name: "sumit",
+    age: 23,
+  };
+  let arr = [1, 3, 4];
+  return (
+    <>
+      <Card username="sumit" btnText={"click me"} obj={obj} newArr={arr} />
+      <Card username="kumar" />
+      <Card username="sam" />
+    </>
+  );
+}
+
+export default App;
