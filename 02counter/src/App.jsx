4ef7acf201f86e4eebbe684e0f7b50e64d831545
sumit kumar <sumit1523@gmail.com>
2024-02-27 20:57:12 +0530

    setting multiple setstates

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -13,6 +13,10 @@ function App() {
   const addValue = () => {
     if (counter < 20) {
       setCounter(counter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
     }
   };
   const removeValue = () => {
