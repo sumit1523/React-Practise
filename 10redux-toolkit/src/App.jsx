c4adf360dc330db6617c7bdcd130d8d1a2103e5f
sumit kumar <sumit1523@gmail.com>
2024-02-28 23:50:24 +0530

    todo with toolokit

--- 10redux-toolkit/src/App.jsx
+++ 10redux-toolkit/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
       <h1>Redux toolkit Todo App</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
